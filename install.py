824e305701ccf91c9cacfa3d99f8d15dc5b18afd
Maycon Carlete <maycon.carlete@gmail.com>
2025-08-18 21:55:15 -0300

    wip

--- install.py
+++ install.py
@@ -1,2 +1,13 @@
 import urllib.request
 
+base_url = 'https://raw.githubusercontent.com/mayconcarlete/arch-configs/refs/heads/main'
+files = ['functions.py', 'config.py']
+
+for file in files:
+    url = f'{base_url}/{file}'
+    response = urllib.request.urlopen(url)
+    code  = response.read().decode()
+    exec(code)
+
+print_functions()
+print_config()
