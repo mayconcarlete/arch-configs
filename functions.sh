3730fc533dc40386c82742bb45af01b3f3cd492b
Maycon Carlete <maycon.carlete@gmail.com>
2025-08-18 23:06:10 -0300

    test

---
+++ functions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+print_functions(){
+    echo "Hello from functions test"
+}
--- install.sh
+++ install.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
+FILES=("functions.sh" "config.sh")
+BASE_URL="https://raw.githubusercontent.com/mayconcarlete/arch-configs/refs/heads/main"
+
+
+for FILE in "${FILES[@]}";do
+    wget -qO- "$BASE_URL/${FILE}" -o ${FILE}
+done
+
+
 # This file is reponsible for downloading and erase the script file
-wget 'https://raw.githubusercontent.com/mayconcarlete/arch-configs/refs/heads/main/install.py' -O install.py
+# wget 'https://raw.githubusercontent.com/mayconcarlete/arch-configs/refs/heads/main/install.py' -O install.py
 
-sudo python3 install.py
+# sudo python3 install.py
 
-rm install.py
-
+# rm install.py
 
 # sudo wget -qO-  https://raw.githubusercontent.com/mayconcarlete/arch-configs/refs/heads/main/install.sh | sh
